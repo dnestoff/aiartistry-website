6eea42ac9e4ae53a34420a937e36d210cf28bdc1
Dave <d.nestoff@gmail.com>
2024-11-22 15:35:09 -0700

    Make a copy of long footer component and create pared down one for initial release

--- src/components/widgets/Footer_full.tsx
+++ src/components/widgets/Footer_full.tsx
@@ -61,7 +61,7 @@ export default component$(() => {
     {
       label: "Github",
       icon: IconGithub,
-      href: "https://www.linkedin.com/in/davidnestoff",
+      href: "https://github.com/onwidget/qwind",
     },
   ];
 
@@ -72,12 +72,58 @@ export default component$(() => {
           <div class="col-span-12 lg:col-span-4 pr-8">
             <div class="mb-2">
               <Link class="inline-block font-bold text-xl" href={"/"}>
-                AI Artistry
+                Qwind
               </Link>
             </div>
             <div class="text-sm text-gray-600 dark:text-gray-400">
-              Copyright AI Artistry 2024. All rights reserved.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi
+              risus tempus nulla
             </div>
+          </div>
+          {links.map(({ title, items }, index) => (
+            <div key={index} class="col-span-6 md:col-span-3 lg:col-span-2">
+              <div class="text-gray-800 dark:text-gray-300 font-medium mb-2">{title}</div>
+              {Array.isArray(items) && items.length > 0 && (
+                <ul class="text-sm">
+                  {items.map(({ title, href }, index2) => (
+                    <li key={index2} class="mb-2">
+                      <Link
+                        class="text-gray-600 hover:text-gray-700 hover:underline dark:text-gray-400 transition duration-150 ease-in-out"
+                        href={href}
+                      >
+                        {title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ))}
+        </div>
+        <div class="md:flex md:items-center md:justify-between py-6 md:py-8">
+          <ul class="flex mb-4 md:order-1 -ml-2 md:ml-4 md:mb-0">
+            {social.map(({ label, href, icon: Icon }, index) => (
+              <li key={index}>
+                <Link
+                  class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
+                  aria-label={label}
+                  title={label}
+                  href={href}
+                >
+                  {typeof Icon !== "undefined" && <Icon />}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <div class="text-sm text-gray-700 mr-4 dark:text-slate-400">
+            <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
+            Made by{" "}
+            <a class="text-secondary-800 underline dark:text-gray-200" href="https://onwidget.com/">
+              {" "}
+              onWidget
+            </a>{" "}
+            · All rights reserved.
           </div>
         </div>
       </div>
